0d8c2507cd4ef3c9e04e12c65cdfc21305daafae
Mohottige Don Isuru Chandika Rathnayaka <125681187+IsuruChandikaRathnayaka@users.noreply.github.com>
2024-04-26 01:45:58 +0530

    made some changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,92 +1,92 @@
+
+
 function Skills() {
     return (
-        <>
+        <div>
         <div id="skills">
             <h2 className="text-center mt-5" ><b>SKILLS</b></h2>
             <div className="row align-items-center d-flex justify-content-center pb-5">
                 <div className="col-md-3">
 
                 <h5 className="mt-5 text-danger"><b>HTML</b></h5>
-                    <div className="progress">
-                        
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "90%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
+                <div className="progress bg-dark">
+          
+        </div>
 
                     <h5 className="mt-4 text-danger"><b>CSS</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                      
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>javascript</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>ReactJs</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>PHP</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>Flask</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                      
                     </div>
                     <h5 className="mt-4 text-danger"><b>nodeJS</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
                 </div>
                 
                 <div className="col-md-3">
                 <h5 className="mt-5 text-danger"><b>Java</b></h5>
-                    <div className="progress">
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress bg-dark">
+                      
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>Python</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>Figma</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                      
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>Boostrap</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>Jquery</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                     <h5 className="mt-4 text-danger"><b>MySQL</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                     <h5 className="mt-4 text-danger"><b>SQlite</b></h5>
-                    <div className="progress">
+                    <div className="progress bg-dark">
                         
-                        <div className="progress-bar bg-dark" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                       
                     </div>
 
                 
@@ -94,7 +94,7 @@ function Skills() {
             </div>
             </div>
        
-        </>
+        </div>
     );
 }
 
